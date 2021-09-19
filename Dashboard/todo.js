da39b7dfaa016db120738af81651d25b6faeba10
Amit-Sheer Cohen <amitcohen95@gmail.com>
2021-09-19 20:00:29 +0300

    add

--- Dashboard/todo.js
+++ Dashboard/todo.js
@@ -1,2 +1,3 @@
 // todo: change the ui to new schema
 // todo: change data in Dashboard/arieldashboard2021withws/public/js/material-dashboard.js to load from my data read from file/database(redis)
+// todo: pubsub to redis server(useful link https://ridwanfajar.medium.com/using-redis-pub-sub-with-node-js-its-quite-easy-c9c8b4dae79f)
