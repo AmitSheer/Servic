8f3a7e1a3954a6768480593bf9a7cca4e189fff6
AmitSheer <amitcohen95@gmail.com>
2021-09-26 11:54:14 +0300

    added different color and changed ui

--- Dashboard/server.js
+++ Dashboard/server.js
@@ -13,10 +13,10 @@ liveReloadServer.server.once("connection", () => {
 });
 let data = {
   cards: [
-    {districtId:"haifa", title: "חיפה", value: 500, unit: "חבילות", fotterIcon: "", fotterText: "נפח ממוצע", icon: "content_copy" },
-    {districtId:"dan", title: "דן", value: 1500, unit: "חבילות", fotterIcon: "", fotterText: "נפח ממוצע", icon: "store" },
-    {districtId:"central", title: "מרכז", value: 3500, unit: "חבילות", fotterIcon: "", fotterText: "נפח ממוצע", icon: "info_outline" },
-    {districtId:"south", title: "דרום", value: 700, unit: "חבילות", fotterIcon: "", fotterText: "נפח ממוצע", icon: "add_shopping_cart" }
+    {districtId:"haifa", title: "חיפה", value: 500, unit: "חבילות", fotterIcon: "", fotterText: "נפח ממוצע", icon: "content_copy", index:1, isShow: 'show'},
+    {districtId:"dan", title: "דן", value: 1500, unit: "חבילות", fotterIcon: "", fotterText: "נפח ממוצע", icon: "store", index:2 ,isShow: ''},
+    {districtId:"central", title: "מרכז", value: 3500, unit: "חבילות", fotterIcon: "", fotterText: "נפח ממוצע", icon: "info_outline" , index:3, isShow: '' },
+    {districtId:"south", title: "דרום", value: 700, unit: "חבילות", fotterIcon: "", fotterText: "נפח ממוצע", icon: "add_shopping_cart" , index:4,isShow: '' }
   ]
 }
 
@@ -33,9 +33,9 @@ app.get('/', (req, res) => {
 })
 
 app.get('/update', (req, res) => {
-  data.cards.push({districtId:"test", title: "צהוב קצין", value: 90000, unit: "חבילות", fotterIcon: "", fotterText: "נפח ממוצע", icon: "add_shopping_cart" })
+  // data.cards.push({districtId:"test", title: "צהוב קצין", value: 90000, unit: "חבילות", fotterIcon: "", fotterText: "נפח ממוצע", icon: "add_shopping_cart" })
   // page.render("pages/dashboard", data);
-  io.emit('newdata',data)
+  io.emit('newdata',"oga boga")
   res.send(page);
 })
 
