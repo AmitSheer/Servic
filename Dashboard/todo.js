9cdc84b76437a23ec7b91f5a5a50752aa03d7c95
Amit-Sheer Cohen <amitcohen95@gmail.com>
2021-10-07 08:19:25 +0300

    tasks and flow of system for updating data after package is marked as arravied at destination

--- Dashboard/todo.js
+++ Dashboard/todo.js
@@ -3,3 +3,5 @@
 // TODO: add websocket to receive data from server(link https://www.pubnub.com/blog/nodejs-websocket-programming-examples/
 //                                                 or transmit json over websocket https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_client_applications )
 //TODO: read from firebase the image and remove it from firebase
+//TODO: connect to firebase -> read list of images -> for each image download -> delete image from firebase 
+//      -> update ad-hoc data -> if updated data send updated data to ui -> move in redis from hot to cold 
