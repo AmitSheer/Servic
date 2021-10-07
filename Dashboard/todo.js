53f6336f6c9b67b420e9b4467490bbf29abb8726
Amit-Sheer Cohen <amitcohen95@gmail.com>
2021-10-07 07:59:31 +0300

    added new todo for firebase, and file for code

--- Dashboard/todo.js
+++ Dashboard/todo.js
@@ -1,5 +1,5 @@
 // TODO: change the ui to new schema
 // TODO: change data in Dashboard/arieldashboard2021withws/public/js/material-dashboard.js to load from my data read from file/database(redis)
-// TODO: pubsub to redis server(useful link https://ridwanfajar.medium.com/using-redis-pub-sub-with-node-js-its-quite-easy-c9c8b4dae79f)
 // TODO: add websocket to receive data from server(link https://www.pubnub.com/blog/nodejs-websocket-programming-examples/
 //                                                 or transmit json over websocket https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_client_applications )
+//TODO: read from firebase the image and remove it from firebase
