d4bf808f9bbd79d3b234791382b4ce8e57d8ba68
AmitSheer <amitcohen95@gmail.com>
2021-10-12 17:07:34 +0300

    fixed and removed unneccesory files

--- Dashboard/public/js/material-dashboard.js
+++ Dashboard/public/js/material-dashboard.js
@@ -315,7 +315,7 @@ md = {
       dataDailySalesChart = {
         labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
         series: [
-          [42, 17, 7, 60, 23, 18, 38]
+          [42, 17, 7, 40, 23, 18, 38]
         ]
       };
 
@@ -406,312 +406,312 @@ md = {
       md.startAnimationForBarChart(websiteViewsChart);
     }
   },
-
-  initMinimizeSidebar: function() {
-
-    $('#minimizeSidebar').click(function() {
-      var $btn = $(this);
-
-      if (md.misc.sidebar_mini_active == true) {
-        $('body').removeClass('sidebar-mini');
-        md.misc.sidebar_mini_active = false;
-      } else {
-        $('body').addClass('sidebar-mini');
-        md.misc.sidebar_mini_active = true;
-      }
-
-      // we simulate the window Resize so the charts will get updated in realtime.
-      var simulateWindowResize = setInterval(function() {
-        window.dispatchEvent(new Event('resize'));
-      }, 180);
-
-      // we stop the simulation of Window Resize after the animations are completed
-      setTimeout(function() {
-        clearInterval(simulateWindowResize);
-      }, 1000);
-    });
-  },
-
-  checkScrollForTransparentNavbar: debounce(function() {
-    if ($(document).scrollTop() > 260) {
-      if (transparent) {
-        transparent = false;
-        $('.navbar-color-on-scroll').removeClass('navbar-transparent');
-      }
-    } else {
-      if (!transparent) {
-        transparent = true;
-        $('.navbar-color-on-scroll').addClass('navbar-transparent');
-      }
-    }
-  }, 17),
-
-
-  initRightMenu: debounce(function() {
-    $sidebar_wrapper = $('.sidebar-wrapper');
-
-    if (!mobile_menu_initialized) {
-      $navbar = $('nav').find('.navbar-collapse').children('.navbar-nav');
-
-      mobile_menu_content = '';
-
-      nav_content = $navbar.html();
-
-      nav_content = '<ul class="nav navbar-nav nav-mobile-menu">' + nav_content + '</ul>';
-
-      navbar_form = $('nav').find('.navbar-form').get(0).outerHTML;
-
-      $sidebar_nav = $sidebar_wrapper.find(' > .nav');
-
-      // insert the navbar form before the sidebar list
-      $nav_content = $(nav_content);
-      $navbar_form = $(navbar_form);
-      $nav_content.insertBefore($sidebar_nav);
-      $navbar_form.insertBefore($nav_content);
-
-      $(".sidebar-wrapper .dropdown .dropdown-menu > li > a").click(function(event) {
-        event.stopPropagation();
-
-      });
-
-      // simulate resize so all the charts/maps will be redrawn
-      window.dispatchEvent(new Event('resize'));
-
-      mobile_menu_initialized = true;
-    } else {
-      if ($(window).width() > 991) {
-        // reset all the additions that we made for the sidebar wrapper only if the screen is bigger than 991px
-        $sidebar_wrapper.find('.navbar-form').remove();
-        $sidebar_wrapper.find('.nav-mobile-menu').remove();
-
-        mobile_menu_initialized = false;
-      }
-    }
-  }, 200),
-
-  startAnimationForLineChart: function(chart) {
-
-    chart.on('draw', function(data) {
-      if (data.type === 'line' || data.type === 'area') {
-        data.element.animate({
-          d: {
-            begin: 600,
-            dur: 700,
-            from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
-            to: data.path.clone().stringify(),
-            easing: Chartist.Svg.Easing.easeOutQuint
-          }
-        });
-      } else if (data.type === 'point') {
-        seq++;
-        data.element.animate({
-          opacity: {
-            begin: seq * delays,
-            dur: durations,
-            from: 0,
-            to: 1,
-            easing: 'ease'
-          }
-        });
-      }
-    });
-
-    seq = 0;
-  },
-  startAnimationForBarChart: function(chart) {
-
-    chart.on('draw', function(data) {
-      if (data.type === 'bar') {
-        seq2++;
-        data.element.animate({
-          opacity: {
-            begin: seq2 * delays2,
-            dur: durations2,
-            from: 0,
-            to: 1,
-            easing: 'ease'
-          }
-        });
-      }
-    });
-
-    seq2 = 0;
-  },
-
-
-  initFullCalendar: function() {
-    $calendar = $('#fullCalendar');
-
-    today = new Date();
-    y = today.getFullYear();
-    m = today.getMonth();
-    d = today.getDate();
-
-    $calendar.fullCalendar({
-      viewRender: function(view, element) {
-        // We make sure that we activate the perfect scrollbar when the view isn't on Month
-        if (view.name != 'month') {
-          $(element).find('.fc-scroller').perfectScrollbar();
-        }
-      },
-      header: {
-        left: 'title',
-        center: 'month,agendaWeek,agendaDay',
-        right: 'prev,next,today'
-      },
-      defaultDate: today,
-      selectable: true,
-      selectHelper: true,
-      views: {
-        month: { // name of view
-          titleFormat: 'MMMM YYYY'
-          // other view-specific options here
-        },
-        week: {
-          titleFormat: " MMMM D YYYY"
-        },
-        day: {
-          titleFormat: 'D MMM, YYYY'
-        }
-      },
-
-      select: function(start, end) {
-
-        // on select we show the Sweet Alert modal with an input
-        swal({
-            title: 'Create an Event',
-            html: '<div class="form-group">' +
-              '<input class="form-control" placeholder="Event Title" id="input-field">' +
-              '</div>',
-            showCancelButton: true,
-            confirmButtonClass: 'btn btn-success',
-            cancelButtonClass: 'btn btn-danger',
-            buttonsStyling: false
-          }).then(function(result) {
-
-            var eventData;
-            event_title = $('#input-field').val();
-
-            if (event_title) {
-              eventData = {
-                title: event_title,
-                start: start,
-                end: end
-              };
-              $calendar.fullCalendar('renderEvent', eventData, true); // stick? = true
-            }
-
-            $calendar.fullCalendar('unselect');
-
-          })
-          .catch(swal.noop);
-      },
-      editable: true,
-      eventLimit: true, // allow "more" link when too many events
-
-
-      // color classes: [ event-blue | event-azure | event-green | event-orange | event-red ]
-      events: [{
-          title: 'All Day Event',
-          start: new Date(y, m, 1),
-          className: 'event-default'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: new Date(y, m, d - 4, 6, 0),
-          allDay: false,
-          className: 'event-rose'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: new Date(y, m, d + 3, 6, 0),
-          allDay: false,
-          className: 'event-rose'
-        },
-        {
-          title: 'Meeting',
-          start: new Date(y, m, d - 1, 10, 30),
-          allDay: false,
-          className: 'event-green'
-        },
-        {
-          title: 'Lunch',
-          start: new Date(y, m, d + 7, 12, 0),
-          end: new Date(y, m, d + 7, 14, 0),
-          allDay: false,
-          className: 'event-red'
-        },
-        {
-          title: 'Md-pro Launch',
-          start: new Date(y, m, d - 2, 12, 0),
-          allDay: true,
-          className: 'event-azure'
-        },
-        {
-          title: 'Birthday Party',
-          start: new Date(y, m, d + 1, 19, 0),
-          end: new Date(y, m, d + 1, 22, 30),
-          allDay: false,
-          className: 'event-azure'
-        },
-        {
-          title: 'Click for Creative Tim',
-          start: new Date(y, m, 21),
-          end: new Date(y, m, 22),
-          url: 'http://www.creative-tim.com/',
-          className: 'event-orange'
-        },
-        {
-          title: 'Click for Google',
-          start: new Date(y, m, 21),
-          end: new Date(y, m, 22),
-          url: 'http://www.creative-tim.com/',
-          className: 'event-orange'
-        }
-      ]
-    });
-  },
-
-  initVectorMap: function() {
-    var mapData = {
-      "AU": 760,
-      "BR": 550,
-      "CA": 120,
-      "DE": 1300,
-      "FR": 540,
-      "GB": 690,
-      "GE": 200,
-      "IN": 200,
-      "RO": 600,
-      "RU": 300,
-      "US": 2920,
-    };
-
-    $('#worldMap').vectorMap({
-      map: 'world_mill_en',
-      backgroundColor: "transparent",
-      zoomOnScroll: false,
-      regionStyle: {
-        initial: {
-          fill: '#e4e4e4',
-          "fill-opacity": 0.9,
-          stroke: 'none',
-          "stroke-width": 0,
-          "stroke-opacity": 0
-        }
-      },
-
-      series: {
-        regions: [{
-          values: mapData,
-          scale: ["#AAAAAA", "#444444"],
-          normalizeFunction: 'polynomial'
-        }]
-      },
-    });
-  }
+  //
+  // initMinimizeSidebar: function() {
+  //
+  //   $('#minimizeSidebar').click(function() {
+  //     var $btn = $(this);
+  //
+  //     if (md.misc.sidebar_mini_active == true) {
+  //       $('body').removeClass('sidebar-mini');
+  //       md.misc.sidebar_mini_active = false;
+  //     } else {
+  //       $('body').addClass('sidebar-mini');
+  //       md.misc.sidebar_mini_active = true;
+  //     }
+  //
+  //     // we simulate the window Resize so the charts will get updated in realtime.
+  //     var simulateWindowResize = setInterval(function() {
+  //       window.dispatchEvent(new Event('resize'));
+  //     }, 180);
+  //
+  //     // we stop the simulation of Window Resize after the animations are completed
+  //     setTimeout(function() {
+  //       clearInterval(simulateWindowResize);
+  //     }, 1000);
+  //   });
+  // },
+  //
+  // checkScrollForTransparentNavbar: debounce(function() {
+  //   if ($(document).scrollTop() > 260) {
+  //     if (transparent) {
+  //       transparent = false;
+  //       $('.navbar-color-on-scroll').removeClass('navbar-transparent');
+  //     }
+  //   } else {
+  //     if (!transparent) {
+  //       transparent = true;
+  //       $('.navbar-color-on-scroll').addClass('navbar-transparent');
+  //     }
+  //   }
+  // }, 17),
+  //
+  //
+  // initRightMenu: debounce(function() {
+  //   $sidebar_wrapper = $('.sidebar-wrapper');
+  //
+  //   if (!mobile_menu_initialized) {
+  //     $navbar = $('nav').find('.navbar-collapse').children('.navbar-nav');
+  //
+  //     mobile_menu_content = '';
+  //
+  //     nav_content = $navbar.html();
+  //
+  //     nav_content = '<ul class="nav navbar-nav nav-mobile-menu">' + nav_content + '</ul>';
+  //
+  //     navbar_form = $('nav').find('.navbar-form').get(0).outerHTML;
+  //
+  //     $sidebar_nav = $sidebar_wrapper.find(' > .nav');
+  //
+  //     // insert the navbar form before the sidebar list
+  //     $nav_content = $(nav_content);
+  //     $navbar_form = $(navbar_form);
+  //     $nav_content.insertBefore($sidebar_nav);
+  //     $navbar_form.insertBefore($nav_content);
+  //
+  //     $(".sidebar-wrapper .dropdown .dropdown-menu > li > a").click(function(event) {
+  //       event.stopPropagation();
+  //
+  //     });
+  //
+  //     // simulate resize so all the charts/maps will be redrawn
+  //     window.dispatchEvent(new Event('resize'));
+  //
+  //     mobile_menu_initialized = true;
+  //   } else {
+  //     if ($(window).width() > 991) {
+  //       // reset all the additions that we made for the sidebar wrapper only if the screen is bigger than 991px
+  //       $sidebar_wrapper.find('.navbar-form').remove();
+  //       $sidebar_wrapper.find('.nav-mobile-menu').remove();
+  //
+  //       mobile_menu_initialized = false;
+  //     }
+  //   }
+  // }, 200),
+  //
+  // startAnimationForLineChart: function(chart) {
+  //
+  //   chart.on('draw', function(data) {
+  //     if (data.type === 'line' || data.type === 'area') {
+  //       data.element.animate({
+  //         d: {
+  //           begin: 600,
+  //           dur: 700,
+  //           from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+  //           to: data.path.clone().stringify(),
+  //           easing: Chartist.Svg.Easing.easeOutQuint
+  //         }
+  //       });
+  //     } else if (data.type === 'point') {
+  //       seq++;
+  //       data.element.animate({
+  //         opacity: {
+  //           begin: seq * delays,
+  //           dur: durations,
+  //           from: 0,
+  //           to: 1,
+  //           easing: 'ease'
+  //         }
+  //       });
+  //     }
+  //   });
+  //
+  //   seq = 0;
+  // },
+  // startAnimationForBarChart: function(chart) {
+  //
+  //   chart.on('draw', function(data) {
+  //     if (data.type === 'bar') {
+  //       seq2++;
+  //       data.element.animate({
+  //         opacity: {
+  //           begin: seq2 * delays2,
+  //           dur: durations2,
+  //           from: 0,
+  //           to: 1,
+  //           easing: 'ease'
+  //         }
+  //       });
+  //     }
+  //   });
+  //
+  //   seq2 = 0;
+  // },
+  //
+  //
+  // initFullCalendar: function() {
+  //   $calendar = $('#fullCalendar');
+  //
+  //   today = new Date();
+  //   y = today.getFullYear();
+  //   m = today.getMonth();
+  //   d = today.getDate();
+  //
+  //   $calendar.fullCalendar({
+  //     viewRender: function(view, element) {
+  //       // We make sure that we activate the perfect scrollbar when the view isn't on Month
+  //       if (view.name != 'month') {
+  //         $(element).find('.fc-scroller').perfectScrollbar();
+  //       }
+  //     },
+  //     header: {
+  //       left: 'title',
+  //       center: 'month,agendaWeek,agendaDay',
+  //       right: 'prev,next,today'
+  //     },
+  //     defaultDate: today,
+  //     selectable: true,
+  //     selectHelper: true,
+  //     views: {
+  //       month: { // name of view
+  //         titleFormat: 'MMMM YYYY'
+  //         // other view-specific options here
+  //       },
+  //       week: {
+  //         titleFormat: " MMMM D YYYY"
+  //       },
+  //       day: {
+  //         titleFormat: 'D MMM, YYYY'
+  //       }
+  //     },
+  //
+  //     select: function(start, end) {
+  //
+  //       // on select we show the Sweet Alert modal with an input
+  //       swal({
+  //           title: 'Create an Event',
+  //           html: '<div class="form-group">' +
+  //             '<input class="form-control" placeholder="Event Title" id="input-field">' +
+  //             '</div>',
+  //           showCancelButton: true,
+  //           confirmButtonClass: 'btn btn-success',
+  //           cancelButtonClass: 'btn btn-danger',
+  //           buttonsStyling: false
+  //         }).then(function(result) {
+  //
+  //           var eventData;
+  //           event_title = $('#input-field').val();
+  //
+  //           if (event_title) {
+  //             eventData = {
+  //               title: event_title,
+  //               start: start,
+  //               end: end
+  //             };
+  //             $calendar.fullCalendar('renderEvent', eventData, true); // stick? = true
+  //           }
+  //
+  //           $calendar.fullCalendar('unselect');
+  //
+  //         })
+  //         .catch(swal.noop);
+  //     },
+  //     editable: true,
+  //     eventLimit: true, // allow "more" link when too many events
+  //
+  //
+  //     // color classes: [ event-blue | event-azure | event-green | event-orange | event-red ]
+  //     events: [{
+  //         title: 'All Day Event',
+  //         start: new Date(y, m, 1),
+  //         className: 'event-default'
+  //       },
+  //       {
+  //         id: 999,
+  //         title: 'Repeating Event',
+  //         start: new Date(y, m, d - 4, 6, 0),
+  //         allDay: false,
+  //         className: 'event-rose'
+  //       },
+  //       {
+  //         id: 999,
+  //         title: 'Repeating Event',
+  //         start: new Date(y, m, d + 3, 6, 0),
+  //         allDay: false,
+  //         className: 'event-rose'
+  //       },
+  //       {
+  //         title: 'Meeting',
+  //         start: new Date(y, m, d - 1, 10, 30),
+  //         allDay: false,
+  //         className: 'event-green'
+  //       },
+  //       {
+  //         title: 'Lunch',
+  //         start: new Date(y, m, d + 7, 12, 0),
+  //         end: new Date(y, m, d + 7, 14, 0),
+  //         allDay: false,
+  //         className: 'event-red'
+  //       },
+  //       {
+  //         title: 'Md-pro Launch',
+  //         start: new Date(y, m, d - 2, 12, 0),
+  //         allDay: true,
+  //         className: 'event-azure'
+  //       },
+  //       {
+  //         title: 'Birthday Party',
+  //         start: new Date(y, m, d + 1, 19, 0),
+  //         end: new Date(y, m, d + 1, 22, 30),
+  //         allDay: false,
+  //         className: 'event-azure'
+  //       },
+  //       {
+  //         title: 'Click for Creative Tim',
+  //         start: new Date(y, m, 21),
+  //         end: new Date(y, m, 22),
+  //         url: 'http://www.creative-tim.com/',
+  //         className: 'event-orange'
+  //       },
+  //       {
+  //         title: 'Click for Google',
+  //         start: new Date(y, m, 21),
+  //         end: new Date(y, m, 22),
+  //         url: 'http://www.creative-tim.com/',
+  //         className: 'event-orange'
+  //       }
+  //     ]
+  //   });
+  // },
+  //
+  // initVectorMap: function() {
+  //   var mapData = {
+  //     "AU": 760,
+  //     "BR": 550,
+  //     "CA": 120,
+  //     "DE": 1300,
+  //     "FR": 540,
+  //     "GB": 690,
+  //     "GE": 200,
+  //     "IN": 200,
+  //     "RO": 600,
+  //     "RU": 300,
+  //     "US": 2920,
+  //   };
+  //
+  //   $('#worldMap').vectorMap({
+  //     map: 'world_mill_en',
+  //     backgroundColor: "transparent",
+  //     zoomOnScroll: false,
+  //     regionStyle: {
+  //       initial: {
+  //         fill: '#e4e4e4',
+  //         "fill-opacity": 0.9,
+  //         stroke: 'none',
+  //         "stroke-width": 0,
+  //         "stroke-opacity": 0
+  //       }
+  //     },
+  //
+  //     series: {
+  //       regions: [{
+  //         values: mapData,
+  //         scale: ["#AAAAAA", "#444444"],
+  //         normalizeFunction: 'polynomial'
+  //       }]
+  //     },
+  //   });
+  // }
 }
 
 // Returns a function, that, as long as it continues to be invoked, will not
