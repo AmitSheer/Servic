15875024d1c1d05edfea3b2a25bd5f5f33c96afa
Amit-Sheer Cohen <amitcohen95@gmail.com>
2021-10-26 19:06:59 +0300

    finishing touches

--- QRSimulator/PackageSender.js
+++ QRSimulator/PackageSender.js
@@ -16,51 +16,30 @@ function randomIndex(i) {
 }
 
 
-// server.get('/', function (req, res) {
-    var i = 1;
-//    while (true) {
-    while(i<20){
-        var packageID = randomIndex(i+100)
+var i = 1;
+while(i<400){
+    var packageID = randomIndex(i+100)
+    var thePackage = new pGenerator.createRandomPackage(packageID);
+    console.log(thePackage)
+    let stringdata = JSON.stringify(thePackage)
+    redisClient.publish("newPackage", stringdata, function () {
+    });
+    qGenerator.createQR(thePackage)
+    packagesOnTheWay[packagesOnTheWay.length] = packageID
+    i++;
+}
 
-        var thePackage = new pGenerator.createRandomPackage(packageID);
-        console.log(thePackage)
-        let stringdata = JSON.stringify(thePackage)
-        redisClient.publish("newPackage", stringdata, function () {
-        });
-
-        qGenerator.createQR(thePackage)
-        // fbSender.sendPackageToFirebase(i)
-        packagesOnTheWay[packagesOnTheWay.length] = packageID
-
-        var random = (Math.round(Math.random()*2500))
-        if(random%2 == 0 && packagesOnTheWay.length>0){
-            var temp = Math.floor(Math.random()*packagesOnTheWay.length)
-            // if(temp = 0) temp = 1
-            // var index = packagesOnTheWay.length % (temp)
-            var tempi = packagesOnTheWay[temp]
-            console.log("+++++++++++++++++++++    "+tempi+"    "+"    "+packagesOnTheWay.length+"   "+temp)
-            fbSender.sendPackageToFirebase(tempi)
-            packagesOnTheWay.splice(temp, 1);
-            console.log("send "+tempi+", curr "+i)
-        }
-        i++;
-
-        // var secondsToWait = 100;
-        // setInterval(checkPackages, secondsToWait*1000)
-        // await sleep(2000);
-
-        
-        // setTimeout(, secondsToWait*1000);
-  }
-// });
-
-// in the lines below we connect to the redis' server
-// redisClient.on('connect', function () {
-//     console.log('Sender connected to Redis');
-// });
-
-
-// in the lines below we connect to the node's server
-// server.listen(6062, function () {
-//     console.log('Sender is running on port 6062');
-// });
+async function deliver(){
+    if(packagesOnTheWay.length==0) return
+    let random = (Math.round(Math.random()*2500))
+    if(random%5 == 0 && packagesOnTheWay.length>0){
+        let temp = Math.floor(Math.random() * packagesOnTheWay.length);
+        let tempi = packagesOnTheWay[temp];
+        console.log("+++++++++++++++++++++    "+tempi+"    "+"    "+packagesOnTheWay.length+"   "+temp)
+        fbSender.sendPackageToFirebase(tempi)
+        packagesOnTheWay.splice(temp, 1);
+        console.log("send "+tempi+", curr "+i)
+    }
+}
+setInterval(deliver,500)
+console.log('done')
